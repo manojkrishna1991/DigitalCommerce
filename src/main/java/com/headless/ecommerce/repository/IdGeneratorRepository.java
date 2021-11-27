8ac7252f80770b7491d51346964ed4b1536a9017
dmanojkrishna@gmail.com <saradhayal123x>
2021-11-27 11:14:22 +0530

    Remove Unwanted URls

--- src/main/java/com/headless/ecommerce/repository/IdGeneratorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.headless.ecommerce.repository;
-
-import com.headless.ecommerce.domain.IdGenerator;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IdGeneratorRepository extends CrudRepository<IdGenerator, String> {
-    IdGenerator findByName(String name);
-}
