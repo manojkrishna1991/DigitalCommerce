d60ead905977ee1cb02d299e2457d8d352494a45
manoj.dhayalan <manoj.dhayalan@mapp.com>
2023-01-09 19:10:32 +0100

    Add test cases to catalog service

--- src/main/java/com/headless/ecommerce/domain/Tracker.java
+++ src/main/java/com/headless/ecommerce/domain/Tracker.java
@@ -1,10 +1,5 @@
 package com.headless.ecommerce.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import java.time.LocalDate;
-
-
 public class Tracker {
 
 
---
+++ src/test/java/com/headless/ecommerce/service/CatalogServiceTest.java
@@ -0,0 +1,39 @@
+package com.headless.ecommerce.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.headless.ecommerce.domain.Catalog;
+import com.headless.ecommerce.domain.CatalogAttributes;
+import com.headless.ecommerce.repository.CatalogRepository;
+@ExtendWith(MockitoExtension.class)
+public class CatalogServiceTest {
+	
+	@Mock
+	private CatalogRepository catalogRepository;
+	@Mock
+	private CatalogService catalogService;
+	
+	@Test
+	public void testSaveCatalog() {
+		List<CatalogAttributes> catalogAttributes = new ArrayList<>();
+		Catalog catalog = new Catalog();
+		catalog.setId(12345L);
+		catalog.setName("test");
+		catalog.setCatalogAttributes(catalogAttributes);
+		when(catalogService.saveCatalog(catalog)).thenReturn(catalog);
+		Catalog outputCatalog = catalogService.saveCatalog(catalog);
+		
+		assertEquals(outputCatalog.getName(), "test");
+		assertEquals(outputCatalog.getId(), 12345L);
+	}
+
+}
