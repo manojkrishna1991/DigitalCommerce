cb27e3369cdc0bafa066c50b059b2226ded4a14d
manoj.dhayalan <manoj.dhayalan@mapp.com>
2023-01-10 19:10:20 +0100

    catalogAttributeMapper and CatalogService test cases

--- src/main/java/com/headless/ecommerce/service/CatalogService.java
+++ src/main/java/com/headless/ecommerce/service/CatalogService.java
@@ -7,7 +7,6 @@ import com.headless.ecommerce.exception.productcatalog.CatalogNotFoundException;
 import com.headless.ecommerce.mapper.CatalogAttributesMapper;
 import com.headless.ecommerce.repository.CatalogAttributeRepository;
 import com.headless.ecommerce.repository.CatalogRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,48 +17,56 @@ import java.util.stream.Collectors;
 
 @Service
 public class CatalogService {
-    @Autowired
-    private CatalogRepository catalogRepository;
-    @Autowired
-    private CatalogAttributeRepository catalogAttributeRepository;
-    @Autowired
-    private CatalogAttributesMapper catalogAttributesMapper;
-    @Transactional
-    public Catalog saveCatalog(Catalog catalog) {
-        return catalogRepository.save(catalog);
-    }
+	private final CatalogRepository catalogRepository;
+	private final CatalogAttributeRepository catalogAttributeRepository;
+	private final CatalogAttributesMapper catalogAttributesMapper;
 
-    public void deleteCatalog(Long catalogId) {
-        catalogRepository.deleteById(catalogId);
-    }
+	public CatalogService(CatalogRepository catalogRepository, CatalogAttributeRepository catalogAttributeRepository,
+			CatalogAttributesMapper catalogAttributesMapper) {
+		this.catalogRepository = catalogRepository;
+		this.catalogAttributeRepository = catalogAttributeRepository;
+		this.catalogAttributesMapper = catalogAttributesMapper;
+	}
 
-    public void deleteCatalogAll() {
-        catalogRepository.deleteAll();
-    }
+	@Transactional
+	public Catalog saveCatalog(Catalog catalog) {
+		return catalogRepository.save(catalog);
+	}
 
-    public Catalog findCatalog(@NonNull Long id) {
-        final Optional<Catalog> catalog = catalogRepository.findById(id);
-        if (!catalog.isPresent()) {
-            throw new CatalogNotFoundException();
-        }
-        return catalog.get();
-    }
+	public void deleteCatalog(Long catalogId) {
+		catalogRepository.deleteById(catalogId);
+	}
 
-    public List<CatalogAttributesDto> saveCatalogAttributes(List<CatalogAttributesDto> sourceCatalogAttributes, @NonNull Long catalogID) {
-        Catalog catalog = findCatalog(catalogID);
-        sourceCatalogAttributes.forEach(attributes -> {
-            CatalogAttributes catalogAttributes = new CatalogAttributes();
-            catalogAttributes.setId(attributes.getId());
-            catalogAttributes.setKey(attributes.getKey());
-            catalogAttributes.setValue(attributes.getValue());
-            catalogAttributes.setCatalog(catalog);
-            catalogAttributeRepository.save(catalogAttributes);
-        });
-        return catalogAttributeRepository.findByCatalog(catalog).stream().map(catalogAttributes -> catalogAttributesMapper.catalogAttributesToCatalogAttributesDto(catalogAttributes)).collect(Collectors.toList());
-    }
+	public void deleteCatalogAll() {
+		catalogRepository.deleteAll();
+	}
 
-    public List<CatalogAttributesDto> getCatalogAttributes(@NonNull Long catalogID) {
-        Catalog catalog = findCatalog(catalogID);
-        return catalogAttributeRepository.findByCatalog(catalog).stream().map(catalogAttributes -> catalogAttributesMapper.catalogAttributesToCatalogAttributesDto(catalogAttributes)).collect(Collectors.toList());
-    }
+	public Catalog findCatalog(@NonNull Long id) {
+		final Optional<Catalog> catalog = catalogRepository.findById(id);
+		if (!catalog.isPresent()) {
+			throw new CatalogNotFoundException();
+		}
+		return catalog.get();
+	}
+
+	public List<CatalogAttributesDto> saveCatalogAttributes(List<CatalogAttributesDto> sourceCatalogAttributes,
+			@NonNull Long catalogID) {
+		Catalog catalog = findCatalog(catalogID);
+		sourceCatalogAttributes.forEach(attributes -> {
+			CatalogAttributes catalogAttributes = new CatalogAttributes();
+			catalogAttributes.setId(attributes.getId());
+			catalogAttributes.setKey(attributes.getKey());
+			catalogAttributes.setValue(attributes.getValue());
+			catalogAttributes.setCatalog(catalog);
+			catalogAttributeRepository.save(catalogAttributes);
+		});
+		return catalogAttributeRepository.findByCatalog(catalog).stream().map(catalogAttributesMapper::catalogAttributesToCatalogAttributesDto).collect(Collectors.toList());
+	}
+
+	public List<CatalogAttributesDto> getCatalogAttributes(@NonNull Long catalogID) {
+		Catalog catalog = findCatalog(catalogID);
+		return catalogAttributeRepository.findByCatalog(catalog).stream().map(
+						catalogAttributesMapper::catalogAttributesToCatalogAttributesDto)
+				.collect(Collectors.toList());
+	}
 }
--- src/test/java/com/headless/ecommerce/service/CatalogServiceTest.java
+++ src/test/java/com/headless/ecommerce/service/CatalogServiceTest.java
@@ -2,12 +2,15 @@ package com.headless.ecommerce.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.headless.ecommerce.mapper.CatalogAttributesMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -15,7 +18,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.headless.ecommerce.domain.Catalog;
 import com.headless.ecommerce.domain.CatalogAttributes;
+import com.headless.ecommerce.dto.CatalogAttributesDto;
 import com.headless.ecommerce.exception.productcatalog.CatalogNotFoundException;
+import com.headless.ecommerce.repository.CatalogAttributeRepository;
 import com.headless.ecommerce.repository.CatalogRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,29 +30,68 @@ public class CatalogServiceTest {
 	private CatalogRepository catalogRepository;
 	@Mock
 	private CatalogService catalogService;
+	@Mock
+	private CatalogAttributeRepository catalogAttributeRepository;
 
-	@Test
-	public void deleteCatalog() {
-		catalogService.deleteCatalog(12345L);
-		when(catalogService.findCatalog(12345L)).thenThrow(new CatalogNotFoundException());
-		CatalogNotFoundException exception = assertThrows(CatalogNotFoundException.class, () -> {
-			catalogService.findCatalog(12345L);
-		});
-	    assertEquals("catalog not found exception", exception.getMessage());
+	@Mock
+	private CatalogAttributesMapper catalogAttributeMapper;
+	private Catalog catalog;
 
-	}
-
-	@Test
-	public void testSaveCatalog() {
+	@BeforeEach
+	public void setUp() {
+		this.catalogService = new CatalogService(catalogRepository, catalogAttributeRepository, catalogAttributeMapper);
 		List<CatalogAttributes> catalogAttributes = new ArrayList<>();
 		Catalog catalog = new Catalog();
 		catalog.setId(12345L);
 		catalog.setName("test");
 		catalog.setCatalogAttributes(catalogAttributes);
+		this.catalog = catalog;
+	}
+
+	@Test
+	public void deleteCatalog() {
+		CatalogNotFoundException exception = assertThrows(CatalogNotFoundException.class, () -> catalogService.findCatalog(12345L));
+		assertEquals("catalog not found exception", exception.getMessage());
+
+	}
+
+	@Test
+	public void testSaveCatalog() {
 		when(catalogService.saveCatalog(catalog)).thenReturn(catalog);
 		Catalog outputCatalog = catalogService.saveCatalog(catalog);
-
 		assertEquals(outputCatalog.getName(), "test");
 		assertEquals(outputCatalog.getId(), 12345L);
+	}
+
+	@Test
+	public void testFindCatalog() {
+		when(catalogRepository.findById(12345L)).thenReturn(Optional.of(catalog));
+		Catalog outputCatalog = catalogService.findCatalog(12345L);
+		assertEquals(outputCatalog.getId(), 12345L);
+		assertEquals(outputCatalog.getName(), "test");
+	}
+
+	@Test
+	public void testSaveCatalogAttributes() {
+		when(catalogRepository.findById(12345L)).thenReturn(Optional.of(catalog));
+		List<CatalogAttributesDto> listOfCatalogAttributes = new ArrayList<>();
+		CatalogAttributesDto catalogAttributes1 = new CatalogAttributesDto();
+		catalogAttributes1.setId(12345L);
+		catalogAttributes1.setKey("desc");
+		catalogAttributes1.setValue("root catalog");
+		listOfCatalogAttributes.add(catalogAttributes1);
+		List<CatalogAttributes> catalogAttributes = new ArrayList<>();
+		CatalogAttributes catalogAttributes2 = new CatalogAttributes();
+		catalogAttributes2.setId(12345L);
+		catalogAttributes2.setKey("desc");
+		catalogAttributes2.setValue("root catalog");
+		catalogAttributes.add(catalogAttributes2);
+		when(catalogAttributeRepository.findByCatalog(any())).thenReturn(catalogAttributes);
+		when(catalogAttributeMapper.catalogAttributesToCatalogAttributesDto(catalogAttributes2)).thenReturn(catalogAttributes1);
+		List<CatalogAttributesDto> catalogAttributesDtos = catalogService.saveCatalogAttributes(listOfCatalogAttributes,
+				12345L);
+		assertEquals(catalogAttributesDtos.get(0).getId(), 12345L);
+		assertEquals(catalogAttributesDtos.get(0).getKey(), "desc");
+		assertEquals(catalogAttributesDtos.get(0).getValue(), "root catalog");
 	}
 }
